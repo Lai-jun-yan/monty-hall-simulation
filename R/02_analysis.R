83b4fbf64e5e574d355983cf229cafa875f96903
Lai Jun Yan <b10601043@g.ntu.edu.tw>
2026-05-07 17:52:32 +0800

    看一下換門之後，勝率的分布

--- R/02_analysis.R
+++ R/02_analysis.R
@@ -50,14 +50,54 @@ y = c(y_con[,1],y_con[,2])
 mean(y)
 var(y)
 
+###sample mean distribution###
+# 先控制var
+change_door_odd_with_control = function(){
+  z = c()
+  for (k in 1:1000){
+    
+    y_con = matrix(0,nrow = 500,ncol = 2)
+    
+    for (i in 1:500){
+      
+      x_con = first_car_control_var(n=2)
+      x = c()
+      x[1] = change_door(x_con[1])
+      x[2] = change_door(x_con[2])
+      for (j in 1:2){
+        y_con[i,j] = x[j] 
+      }
+    }
+    
+    y = c(y_con[,1],y_con[,2])
+    z[k] = mean(y)
+    
+  }
+  result = c(mean(z),var(z))
+}
 
+x = change_door_odd_with_control()
+x
 
+# 不控制var
+change_door_odd = function(){
+  z = c()
+  for (j in 1:1000){
+    x = first_car(n=1000)
+    y = c()
+    for (i in 1:1000){
+      y[i] = change_door(x[i])
+    }
+    z[j] = mean(y)
+  }
+  result = c(mean(z),var(z))
+}
 
+y = change_door_odd()
 
+Compare_sample_odd_mean_distribution = rbind(
+  "控制" = x,
+  "不控制" = y
+)
 
-
-
-
-
-
-
+Compare_sample_odd_mean_distribution
