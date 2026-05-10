5c6415702fce01f8c7ca6a88a23bc2df03bda731
Lai Jun Yan <b10601043@g.ntu.edu.tw>
2026-05-10 10:57:03 +0800

    畫出第一次選擇的三種變數分布，以進行比較

--- R/04_plot.R
+++ R/04_plot.R
@@ -1,4 +1,112 @@
+source("01_create_first_car_function.R")
+source("02_analysis.R")
+
 ###將分布視覺化###
+
+# 用內建R函數產生第一次選擇，而選到車的機率之估計值分布
+
+R_defuilt = R_defuilt_distribution()
+
+# 畫出 sample mean 的分布
+hist(
+  R_defuilt$distribution,
+  breaks = 30,
+  probability = TRUE,
+  main = "Distribution of Sample Mean",
+  xlab = "Sample Mean"
+)
+
+# 加上理論常態曲線
+curve(
+  dnorm(
+    x,
+    mean = p,
+    sd = sqrt(p*(1-p)/1000)
+  ),
+  add = TRUE,
+  lwd = 2
+)
+
+# 用Inverse function產生第一次選擇，而選到車的機率之估計值分布
+
+Inverse = Inverse_distribution()
+
+hist(
+  Inverse$distribution,
+  breaks = 30,
+  probability = TRUE,
+  main = "Inverse Function Sample Mean Distribution",
+  xlab = "Sample Mean"
+)
+
+curve(
+  dnorm(
+    x,
+    mean = p,
+    sd = sqrt(p*(1-p)/1000)
+  ),
+  add = TRUE,
+  lwd = 2
+)
+
+# 用control產生分布
+
+control = control_distribution()
+
+hist(
+  control$distribution,
+  probability = TRUE,
+  breaks = 30,
+  main = "Control Variance Distribution",
+  xlab = "Sample Mean"
+)
+
+curve(
+  dnorm(
+    x,
+    mean = p,
+    sd = sqrt(p*(1-p)/1000)
+  ),
+  add = TRUE,
+  lwd = 2
+)
+
+# 畫出三種方法的 density curve來比較
+
+plot(
+  density(R_defuilt$distribution),
+  lwd = 2,
+  col = "red",
+  main = "Comparison of Sample Mean Distributions",
+  xlab = "Sample Mean",
+  ylim = c(0, 120)
+)
+
+lines(
+  density(Inverse$distribution),
+  lwd = 2,
+  lty = 2,
+  col = "blue"
+)
+
+lines(
+  density(control$distribution),
+  lwd = 2,
+  lty = 3,
+  col = "darkgreen"
+)
+
+legend(
+  "topright",
+  legend = c(
+    "rbinom",
+    "inverse function",
+    "control variance"
+  ),
+  col = c("red", "blue", "darkgreen"),
+  lwd = 2,
+  lty = c(1,2,3)
+)
 
 
 
