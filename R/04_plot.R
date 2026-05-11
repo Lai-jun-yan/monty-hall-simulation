7b07d32895fe8d6f2fa9780c6e6df04d4126c79b
Lai Jun Yan <b10601043@g.ntu.edu.tw>
2026-05-11 10:01:28 +0800

    畫出換門之後選到跑車的機率分布圖

--- R/04_plot.R
+++ R/04_plot.R
@@ -3,6 +3,8 @@ source("02_analysis.R")
 
 ###將分布視覺化###
 
+## 第一次選擇 ##
+
 # 用內建R函數產生第一次選擇，而選到車的機率之估計值分布
 
 R_defuilt = R_defuilt_distribution()
@@ -111,6 +113,97 @@ legend(
   lwd = 2,
   lty = c(1,2,3)
 )
+
+##--------##
+
+## 第二次選擇 ##
+
+# 換門之後而選到跑車的機率分布
+
+z = change_door_odd_R()
+
+# 畫出 sample mean 的分布
+hist(
+  z$distribution,
+  breaks = 30,
+  probability = TRUE,
+  main = "Distribution of Sampling Distribution of 
+  the Estimated Winning Probability (R Bernoulli Simulation)",
+  xlab = "Probability"
+)
+
+# inverse function
+y = change_door_odd()
+
+hist(
+  y$distribution,
+  breaks = 30,
+  probability = TRUE,
+  main = "Distribution of Sampling Distribution of 
+  the Estimated Winning Probability(Inverse Function)",
+  xlab = "Probability"
+)
+
+# control
+x = change_door_odd_with_control()
+
+hist(
+  x$distribution,
+  probability = TRUE,
+  breaks = 30,
+  main = "Distribution of Sampling Distribution of 
+  the Estimated Winning Probability(Inverse Function with 
+  Control Variance)",
+  xlab = "Probability"
+)
+
+# 畫出三種方法的 density curve來比較
+
+plot(
+  density(z$distribution),
+  lwd = 2,
+  col = "red",
+  main = "Comparison of three Probability Distributions",
+  xlab = "Probability",
+  ylim = c(0, 120)
+)
+
+lines(
+  density(y$distribution),
+  lwd = 2,
+  lty = 2,
+  col = "blue"
+)
+
+lines(
+  density(x$distribution),
+  lwd = 2,
+  lty = 3,
+  col = "darkgreen"
+)
+
+legend(
+  "topright",
+  legend = c(
+    "rbinom",
+    "inverse function",
+    "control variance"
+  ),
+  col = c("red", "blue", "darkgreen"),
+  lwd = 2,
+  lty = c(1,2,3)
+)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
